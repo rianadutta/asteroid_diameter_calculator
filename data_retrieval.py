008db60e817de61caa375b07dc2919e914cb6a2f
Rylan Donohoe <rylan.donohoe@outlook.com>
2022-11-06 13:09:21 -0500

    final

--- data_retrieval.py
+++ data_retrieval.py
@@ -3,7 +3,7 @@ import pandas as pd
 # retrieve data
 asteroid=pd.read_csv('Asteroid.csv')
 
-# preliminarily process data
+# preliminarily data processing
 for p in asteroid:
     asteroid=asteroid[asteroid[p].notnull()]
 print(len(asteroid))
